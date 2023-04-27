96575d823e7a83f728224f1fb9d79a1bbb674055
Hilla Shahrabani <hilla.sh@gmail.com>
2023-04-28 01:24:37 +0300

    save to csv

--- DTO/post.py
+++ DTO/post.py
@@ -1,34 +1,51 @@
+import json
 from datetime import datetime
 
 
 class FacebookPost:
+    CSV_HEADER = ["username", "post_text", "shared_text", "timestamp"]
+
     def __init__(self, username: str, post_text: str, shared_text: str, timestamp: datetime):
         self.username = username
         self.post_text = post_text
         self.shared_text = shared_text
-        self.timestamp = timestamp
+        self.timestamp = timestamp.strftime('%d/%m/%Y')
 
-    def __repr__(self):
+    def to_dict(self):
         return {
             "username": self.username,
             "post_text": self.post_text,
             "shared_text": self.shared_text,
-            "timestamp": self.timestamp
+            "timestamp":  self.timestamp
         }
+
+    def to_csv_line(self):
+        return f"{self.username},\"{self.post_text}\",\"{self.shared_text}\",{self.timestamp}"
+
+    def __repr__(self):
+        return json.dumps(self.to_dict())
 
 
 class TwitterPost:
+    CSV_HEADER = ["username", "post_text", "shared_text", "timestamp"]
+
     def __init__(self, username: str, post_text: str, shared_text: str, timestamp: datetime, pid: int):
         self.id = pid
         self.username = username
         self.post_text = post_text
         self.shared_text = shared_text
-        self.timestamp = timestamp
+        self.timestamp = timestamp.strftime('%d/%m/%Y')
 
-    def __repr__(self):
+    def to_dict(self):
         return {
             "username": self.username,
             "post_text": self.post_text,
             "shared_text": self.shared_text,
             "timestamp": self.timestamp
         }
+
+    def to_csv_line(self):
+        return f"{self.username},{self.post_text},{self.shared_text},{self.timestamp}"
+
+    def __repr__(self):
+        return json.dumps(self.to_dict())
--- routers/scraping.py
+++ routers/scraping.py
@@ -3,6 +3,7 @@ from facebook_scraper import get_posts
 from datetime import datetime
 
 from DTO.post import FacebookPost
+from services.csv_helper import CSVWriter
 
 router = APIRouter(
     prefix="/scraping",
@@ -12,29 +13,33 @@ router = APIRouter(
 )
 
 
-def fetch_posts_for_id(id):
-    posts_itr = get_posts(id, pages=10, options={"posts_per_page": 10})
+def fetch_posts_for_id(ids):
     posts = []
-    for post in posts_itr:
-        posts.append(
-            FacebookPost(
-                username=post.get("username"),
-                post_text=post.get("post_text"),
-                shared_text=post.get("shared_text"),
-                timestamp=datetime.fromtimestamp(post.get("timestamp"))
+    for id in ids:
+        id = id.strip()
+        posts_itr = get_posts(id, pages=10, options={"posts_per_page": 10})
+        for post in posts_itr:
+            posts.append(
+                FacebookPost(
+                    username=post.get("username"),
+                    post_text=post.get("post_text"),
+                    shared_text=post.get("shared_text"),
+                    timestamp=datetime.fromtimestamp(post.get("timestamp"))
+                )
             )
-        )
     return posts
 
 
 def read_ids_from_file(filename):
     with open(filename) as f:
         lines = f.readlines()
-        return lines[0]
+        return lines
 
 
 @router.get("/facebook")
 async def scrape_facebook():
     ids = read_ids_from_file("facebook_ids_for_scraping.txt")
     posts = fetch_posts_for_id(ids)
+    csv_writer = CSVWriter(FacebookPost)
+    csv_writer.write(posts, f"storage/facebook/{datetime.date(datetime.now())}.csv")
     return posts

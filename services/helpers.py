76a6f71f49bdd5f6632e46f3b468b508a2edd5e6
Hilla Shahrabani <hilla.sh@gmail.com>
2023-04-27 20:44:20 +0300

    send mail (with test data)

---
+++ services/helpers.py
@@ -0,0 +1,32 @@
+import os.path
+import yagmail
+
+
+nily_email = 'nily@maavarim.org'
+email_pass = 'pswhnpxqststlrvd'
+
+# path example
+path = r"C:\Users\Administrator\PycharmProjects\d40_dyke\transphobia-detector\routers\testgmail.csv"
+
+
+def send_yagmail():
+    try:
+        yagmail.register(nily_email, email_pass)
+        receiver = "galdel9@gmail.com"
+        body = "TEST Yagmail"
+        # real path of csv file
+        filename = path
+
+        yag = yagmail.SMTP(nily_email)
+        yag.send(
+            to=receiver,
+            subject="Yagmail test with attachment",
+            contents=body,
+            attachments=filename,
+        )
+        print('sent')
+    except Exception as err:
+        print('send_yagmail err: ', err)
+
+
+send_yagmail()

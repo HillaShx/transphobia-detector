6ea4287ba528c8ad53c6cb2819f92ea0043ee3d9
Shahar Levy <shahar.levy@samsung.com>
2023-04-28 00:02:16 +0300

    Add Tweets scraping

--- routers/scraping.py
+++ routers/scraping.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from facebook_scraper import get_posts
+import tweepy
+import config
+import pandas as pd
 
 router = APIRouter(
     prefix="/scraping",
@@ -15,11 +18,31 @@ def fetch_posts_for_id(id):
 def read_ids_from_file(filename):
     with open(filename) as f:
         lines = f.readlines()
-        return lines[0]
+        return [l.rstrip('\n') for l in lines]
 
 @router.get("/facebook")
 async def scrape_facebook():
     ids = read_ids_from_file("facebook_ids_for_scraping.txt")
     posts = fetch_posts_for_id(ids)
     print(posts)
+    return
+
+@router.get("/twitter")
+async def scrape_twitter():
+    ids = read_ids_from_file("twitter_ids_for_scraping.txt")
+    df = pd.DataFrame(columns=["id", "username", "text", "created_at", "lang", "possibly_sensitive"])
+    client = tweepy.Client(bearer_token=config.bearer_token)
+    for id in ids:
+        new_tweets = client.get_users_tweets(id, tweet_fields=['id', 'text', 'possibly_sensitive', 'lang', 'created_at'])
+        user = client.get_user(id=id)
+        for tweet in new_tweets.data:
+            l = {
+                "id": tweet.id,
+                "username": user.data.username,
+                "text": tweet.text,
+                "created_at": tweet.created_at,
+                "lang": "he" if tweet.lang == "iw" else tweet.lang,
+                "possibly_sensitive": tweet.possibly_sensitive,
+            }
+            df = pd.concat([df, pd.DataFrame([l])], ignore_index=True)
     return

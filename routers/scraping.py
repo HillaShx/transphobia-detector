fd1b4bff21c2293344c1da9621f43c340f0e1e21
Hilla Shahrabani <hilla.sh@gmail.com>
2023-04-27 20:33:15 +0300

    add dtos

--- routers/scraping.py
+++ routers/scraping.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from facebook_scraper import get_posts
+from datetime import datetime
+
+from DTO.post import FacebookPost
 
 router = APIRouter(
     prefix="/scraping",
@@ -8,18 +11,30 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 def fetch_posts_for_id(id):
     posts_itr = get_posts(id, pages=10, options={"posts_per_page": 10})
-    return [post for post in posts_itr]
+    posts = []
+    for post in posts_itr:
+        posts.append(
+            FacebookPost(
+                username=post.get("username"),
+                post_text=post.get("post_text"),
+                shared_text=post.get("shared_text"),
+                timestamp=datetime.fromtimestamp(post.get("timestamp"))
+            )
+        )
+    return posts
+
 
 def read_ids_from_file(filename):
     with open(filename) as f:
         lines = f.readlines()
         return lines[0]
 
+
 @router.get("/facebook")
 async def scrape_facebook():
     ids = read_ids_from_file("facebook_ids_for_scraping.txt")
     posts = fetch_posts_for_id(ids)
-    print(posts)
-    return
+    return posts

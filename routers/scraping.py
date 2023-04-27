5cb5b1dca402bd2c757799f4732e0e3e7d28bc3b
Tamar Kalir <tkalir@gmail.com>
2023-04-27 19:01:54 +0300

    adding facebook scraping

--- routers/scraping.py
+++ routers/scraping.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from facebook_scraper import get_posts
 
 router = APIRouter(
     prefix="/scraping",
@@ -7,8 +8,18 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+def fetch_posts_for_id(id):
+    posts_itr = get_posts(id, pages=10, options={"posts_per_page": 10})
+    return [post for post in posts_itr]
 
-@router.get("")
-async def test():
+def read_ids_from_file(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+        return lines[0]
 
-    return {"message": "hello"}
+@router.get("/facebook")
+async def scrape_facebook():
+    ids = read_ids_from_file("facebook_ids_for_scraping.txt")
+    posts = fetch_posts_for_id(ids)
+    print(posts)
+    return
